46134062019de5eab6667792689f404e773f8a7c
Hiro Nonoyama <H@Hiro-no-MacBook-Pro.local>
2015-11-29 14:25:02 +0900

    favoriteページ

--- app/helpers/top_helper.rb
+++ app/helpers/top_helper.rb
@@ -2,7 +2,7 @@ module TopHelper
 
 	def user_favorite_check(user, photo_id)
 		if user
-			if Favorite.where(user_id:user.id).where(photo_id: photo_id).exists?
+			if Favorite.where(user_id:user.id).where(photo_id:photo_id).exists?
 				return true
 			else
 				return false
@@ -12,5 +12,5 @@ module TopHelper
 		end
 	end
 
-	
+
 end
